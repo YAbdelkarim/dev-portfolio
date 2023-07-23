9c6bec23b2c91471540f2a0cb60fbcd2c31bce28
Youssef Abdelkarim <youssefabdelkarim02@gmail.com>
2023-07-23 18:04:16 +0300

    Updates

---
+++ precache-manifest.cbb5a64a08b4cd453d1ef49dae5d6ae0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "06b339493bd6deeca4df",
+    "url": "/dev-portfolio/static/css/main.d355a6aa.chunk.css"
+  },
+  {
+    "revision": "06b339493bd6deeca4df",
+    "url": "/dev-portfolio/static/js/main.06b33949.chunk.js"
+  },
+  {
+    "revision": "b3dcce315e9385ee4a1a",
+    "url": "/dev-portfolio/static/js/1.b3dcce31.chunk.js"
+  },
+  {
+    "revision": "790188b95d2ce1906d45",
+    "url": "/dev-portfolio/static/js/runtime~main.790188b9.js"
+  },
+  {
+    "revision": "20727a26a03318a8f512c7476453638c",
+    "url": "/dev-portfolio/index.html"
+  }
+];
